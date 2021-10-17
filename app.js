9932919c69e247c4efe09e2e1a0d5bfb8679d143
高雄皮咖稱 <56230073+bowbow19@users.noreply.github.com>
2021-10-17 18:03:49 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const port = 3000
 const app = express()
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+
+
 
 app.get('/' , (req,res) =>{
 res.send(`slkdjlskdgdfjgkfjs`)
@@ -8,5 +25,5 @@ res.send(`slkdjlskdgdfjgkfjs`)
 })
 
 app.listen(port , () =>{
-    console.log(`Express is listening to localhost:${port}`);
+    console.log(`Express is listening to http://localhost:${port}`);
 })
