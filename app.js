ebe238609e4bd4283b1bd537332fb47aa527674e
高雄皮咖稱 <56230073+bowbow19@users.noreply.github.com>
2021-10-17 19:58:33 +0800

    feat: add update todo function

--- app.js
+++ app.js
@@ -55,6 +55,32 @@ app.get('/todos/:id' , (req,res) =>{
   .catch(error => console.log(error))
 })
 
+// 讀取要 編輯 的畫面清單
+
+app.get('/todos/:id/edit' , (req,res) =>{
+  const id = req.params.id
+
+  return Todo.findById(id)
+  .lean()
+  .then((todo) =>res.render('edit' , {todo}))
+  .catch(error => console.log(error))
+})
+
+// 編輯 修改功能
+
+app.post('/todos/:id/edit' , (req,res) =>{
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+  
+  .then((todo) =>
+  {
+    todo.name = name
+    return todo.save()
+  })
+  .then(() => res.redirect(`/todos/${id}`))
+  .catch(error => console.log(error))
+})
 
 
 
